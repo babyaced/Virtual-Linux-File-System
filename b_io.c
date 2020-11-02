6d53772273aa6b4631ae8366e887f1d6be0b40e4
JayJaber <calijayjaber@gmail.com>
2020-11-02 14:14:38 -0800

    updated b_io.c template

--- b_io.c
+++ b_io.c
@@ -19,24 +19,25 @@
 #define B_CHUNK_SIZE 512
 #define MAX_OPEN_FILES 20
 
-struct openFileTable{
-    int linuxFD;
+typedef struct fd {
+    int lbaPosition;
     char* buffer;
     int ourBufferOffset;
     int bytesInBuffer;
-};
+}fd;
 
-struct openFileTable openFileTables[MAX_OPEN_FILES]; 
+fd openFileTables[MAX_OPEN_FILES]; // fd is index in fd openFileTables[]
 
 int areWeInitialized = 0;	//Indicates that this has not been initialized
 
 void b_init()
 {
+
 	areWeInitialized = 1;
 	for(int i = 0; i < MAX_OPEN_FILES; i++)
 	{
 		//do something with oftables
-		openFileTables[i].linuxFD = -1;
+		openFileTables[i].lbaPosition = -1;
 		openFileTables[i].bytesInBuffer = 0;
 		openFileTables[i].ourBufferOffset = 0;
 	}
@@ -45,6 +46,8 @@ void b_init()
 
 int b_open (char* filename, int flags){  //cannot open directory
     //startPartitionSystem?
+	
+	// store the lbaPosition in the fd struct  
     char dirName[255];
 	char baseName[255];  
     strcpy(dirName,filename); //makes copy of filename to find dirName //dirname() is destructive!
@@ -74,7 +77,7 @@ int b_read (int fd, char * buffer, int count)  //this is copy of bierman's versi
 		return (-1); 					//invalid file descriptor
 		}
 		
-	if (openFileTables[fd].linuxFD == -1)		//File not open for this descriptor
+	if (openFileTables[fd].lbaPosition == -1)		//File not open for this descriptor
 		{
 		return -1;
 		}	
@@ -106,13 +109,13 @@ int b_read (int fd, char * buffer, int count)  //this is copy of bierman's versi
 		if (part2 > B_CHUNK_SIZE)
 			{
 			int blocks = part2 / B_CHUNK_SIZE; // calculate number of blocks they want
-			bytesRead = LBAread (buffer+part1,blocks,openFileTables[fd].linuxFD);
+			bytesRead = LBAread (buffer+part1,blocks,openFileTables[fd].lbaPosition);
 			part3 = bytesRead;
 			part2 = part2 - part3;  //part 2 is now < B_CHUNK_SIZE, or file is exusted
 			}				
 		
 		//try to read B_CHUNK_SIZE bytes into our buffer
-		bytesRead = LBAread (openFileTables[fd].buffer,1, openFileTables[fd].linuxFD);  //keep as 1 block for now
+		bytesRead = LBAread (openFileTables[fd].buffer,1, openFileTables[fd].lbaPosition);  //keep as 1 block for now
 		
 		// error handling here...  if read fails
 		
@@ -133,7 +136,7 @@ int b_read (int fd, char * buffer, int count)  //this is copy of bierman's versi
 }
 
 int b_write (int fd, char * buffer, int count){
-	
+/*
     if (areWeInitialized == 0) 
 	b_init();  //Initialize system for write
 
@@ -145,11 +148,11 @@ int b_write (int fd, char * buffer, int count){
     if (openFileTables[fd].linuxFD == -1) {	//File not open for this descriptor
 		return -1;
     }
-	
-    // get block position from fd struct
+*/
+    // get block position from oft struct
     int lbaPosition = 1;
     // int count converts to lbaCount
-    int lbaCount = 4;
+    int lbaCount = 4;// test
     int retVal = LBAwrite(buffer, lbaCount, lbaPosition);
     return retVal;
 }
